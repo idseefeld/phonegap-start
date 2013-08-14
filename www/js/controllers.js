aee16968b6b2249f2e31f611056df8ec06816994
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2013-08-14 17:31:10 +0200

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,7 +5,7 @@
 function PhoneListCtrl($scope, $http) {
     $scope.code = null;
     $scope.response = null;
-    $http.jsonp('http://dseefeld65.dyndns.org/phone.aspx?callback=JSON_CALLBACK&test=5').success(function (data) {
+    $http.jsonp('http://dseefeld65.dyndns.org/phone.aspx?callback=JSON_CALLBACK&test=x123').success(function (data) {
         $scope.phones = data;
     }).error(function (data, status, headers, config) {
         $scope.configUrl = config.url;
