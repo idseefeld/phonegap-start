bc38a43b7ce51cf17824e90c3a7f6c76e3639b5c
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2013-08-14 14:39:53 +0200

    update to jsonp

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,11 +3,13 @@
 /* Controllers */
 
 function PhoneListCtrl($scope, $http) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+    $scope.code = null;
+    $scope.response = null;
+    $http.jsonp('http://www.idseefeld.de/phones/phones-2.js?callback=JSON_CALLBACK&test=4').success(function (data) {
+        $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
+    $scope.orderProp = 'age';
 }
 
 //PhoneListCtrl.$inject = ['$scope', '$http'];
